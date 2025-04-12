96df00f5dd4f623e6af35252a1318d5273d298ed
Juliano <julianoprado98@gmail.com>
2025-04-12 01:52:10 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/avaliacaospringboot/AvaliacaospringbootApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.avaliacaospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/avaliacaospringboot/AvaliacaospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.avaliacaospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
