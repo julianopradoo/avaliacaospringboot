b89aa7e17ce7e39bfc15058153513261d1903d75
Juliano Prado <125655127+julianopradoo@users.noreply.github.com>
2025-04-15 06:24:36 +0000

    finalizando

--- src/main/java/br/gov/sp/fatec/avaliacaospringboot/controller/TrabalhoController.java
+++ src/main/java/br/gov/sp/fatec/avaliacaospringboot/controller/TrabalhoController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +27,13 @@ public class TrabalhoController {
         return service.buscarTodos();
     }
     
+    @PostMapping
+    public Trabalho novoTrabalho(@RequestBody Trabalho trabalho) {
+        return service.novo(trabalho);
+    }
+
+    @GetMapping (value = "/{titulo}/{nota}")
+    public List<Trabalho> buscarPorTituloENota(@PathVariable("titulo") String titulo, @PathVariable("nota") Integer nota) {
+        return service.buscarPorTituloENota(titulo, nota);
+    } 
 }
--- src/main/java/br/gov/sp/fatec/avaliacaospringboot/service/TrabalhoService.java
+++ src/main/java/br/gov/sp/fatec/avaliacaospringboot/service/TrabalhoService.java
@@ -1,9 +1,12 @@
 package br.gov.sp.fatec.avaliacaospringboot.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.gov.sp.fatec.avaliacaospringboot.entity.Trabalho;
 import br.gov.sp.fatec.avaliacaospringboot.repository.TrabalhoRepository;
@@ -16,5 +19,24 @@ public class TrabalhoService {
 
     public List<Trabalho> buscarTodos() {
         return repo.findAll();
+    }
+
+    //inserir um novo
+    public Trabalho novo(Trabalho trabalho) {
+        if(trabalho.getTitulo() == null ||
+            trabalho.getTitulo().isBlank() ||
+            trabalho.getGrupo() == null ||
+            trabalho.getGrupo().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Faltou informação");
+        }
+        if(trabalho.getDataHoraEntrega() == null) {
+            trabalho.setDataHoraEntrega(LocalDateTime.now());
+        }
+        return repo.save(trabalho);
+    }
+
+
+    public List<Trabalho> buscarPorTituloENota(String titulo, Integer nota) {
+        return repo.buscarPorTituloENota(titulo, nota);
     }
 }
